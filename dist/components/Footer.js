d139cf199a61f08f02fbdb680fef9a5c70826c69
jinseok_kim <genuineseok@gmail.com>
2022-09-06 14:31:27 +0900

    [Fix] 빌드 폴더/파일 제거

--- dist/components/Footer.js
+++
@@ -1,6 +0,0 @@
-import ComponentImp from "../core/Component.js";
-export default class Footer extends ComponentImp {
-    template() {
-        return `<div class="footer--container">ⓒcreate date Aug . 2022 github jns30k</div>`;
-    }
-}
--- dist/main.js
+++
@@ -1,2 +0,0 @@
-import App from "./app.js";
-new App(document.querySelector("#app"));
